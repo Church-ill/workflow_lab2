6bace074e639ee01889bfb2bf01f43657bbe3ff5
Ben <benjamin.a.spencer@gmail.com>
2015-11-16 13:14:54 -0500

    adds click handler and loadTemplate

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict'
+
+var template = Handlebars.compile($('#data-loader').html());
+
+$(document).ready(function() {
+  var loadTemplate = function(error, data){
+   if (error) {
+     console.log(error);
+   }else {
+     console.log(data);
+     var rowHTML = template({row: data});
+     $("#git-table").html(rowHTML);
+   }
+ };
+
+  $('.load-btn').on ('submit', function(e) {
+    e.preventDefault();
+    api(loadTemplate);
+  });
+
+});
+
+
+
+
